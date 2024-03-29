114b8258cbb71c133b12bae1ed8c4c31528cd4af
Jeremiah Daniels <jeremiahdaniels@2004@gmail.com>
2024-03-29 13:41:44 -0400

    updated setup file that connects

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-import _mysql_connector
+import mysql.connector as ms
 
 try:
-    connection = _mysql_connector.connect(
+    mydb = ms.connect(
         host = "127.0.0.1",
         user = "root",
         password = "Admin1234",
         database = "db4710",
     )
-    if connection.is_connected():
+    if mydb.is_connected():
         print("Sucess!")
+        mycur=mydb.cursor()
+        #mycur.execute("Create Table testUser(MemID int primary key auto_increment, fName VarChar(100), lName VarChar(100), gender VarChar(10), DOB date, height float, activity VarChar(100), weight float, goal VarChar(100))")
+        mycur.execute("show tables")
+        for x in mycur:
+            print(x)    
     
 except _mysql_connector.Error as e:
     print("Error", e)
