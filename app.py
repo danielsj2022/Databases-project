52f90c23ff74c5e404d2ac8d48b888373581cb3f
Jeremiah Daniels <jeremiahdaniels@2004@gmail.com>
2024-03-29 14:25:55 -0400

    app.py contains main program with templates html

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import mysql.connector as ms
+
+#login screen
+#home page
+# show playlist
+#show fav artist
+#find songs based off artist
+#find songs
+#add/delete songs
+#search playlist
+#reccomend songs based off liked artists
+
+#login screen
+#users can see music playlist and liked artists
+#music playlist: add, delete, search
+#liked artist:add
+#reccommend songs based off liked artists
+
+if __name__ == "__main__":
+    app.run(debug = True)
